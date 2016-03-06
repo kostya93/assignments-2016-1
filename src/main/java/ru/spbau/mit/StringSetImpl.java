f0d8219d3f5bc7d02669f250b1418bb294ddefac
Charkin Kostya <93kostya@gmail.com>
2016-03-06 21:15:12 +0300

    del sym

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -17,16 +17,16 @@ public class StringSetImpl implements StringSet{
         Node currentNode = root;
 
         for (int i = 0; i < element.length(); i++) {
+            currentNode.size++;
             int ind = getIndex(element, i);
             if (currentNode.children[ind] == null) {
-                currentNode.children[ind] = new Node(ind);
-                currentNode.children[ind].parent = currentNode;
+                currentNode.children[ind] = new Node();
             }
             currentNode = currentNode.children[ind];
         }
 
+        currentNode.size++;
         currentNode.endOfString = true;
-        incSize(currentNode);
 
         return true;
     }
@@ -43,28 +43,35 @@ public class StringSetImpl implements StringSet{
             return false;
         }
 
-        if (currentNode.endOfString) {
-            return true;
-        }
-
-        return false;
+        return currentNode.endOfString;
     }
 
     @Override
     public boolean remove(String element) {
-        Node currentNode = goDown(root, element);
-
-        if (currentNode == null) {
+        if (!contains(element)) {
             return false;
         }
 
-        if (currentNode.endOfString) {
-            currentNode.endOfString = false;
-            decSize(currentNode);
+        if (element.equals("")) {
+            root.size--;
+            root.endOfString = false;
             return true;
         }
 
-        return false;
+        Node currentNode = root;
+
+        for (int i = 0; i < element.length(); i++) {
+            currentNode.size--;
+            int ind = getIndex(element, i);
+            if (currentNode.children[ind].size == 1) {
+                currentNode.children[ind] = null;
+                return true;
+            }
+            currentNode = currentNode.children[ind];
+        }
+        currentNode.endOfString = false;
+        currentNode.size--;
+        return true;
     }
 
     @Override
@@ -79,28 +86,6 @@ public class StringSetImpl implements StringSet{
             return 0;
         }
         return currentNode.size;
-    }
-
-    private void incSize(Node currentNode) {
-        while (currentNode != root) {
-            currentNode.size++;
-            currentNode = currentNode.parent;
-        }
-        currentNode.size++;
-    }
-
-    private void decSize(Node currentNode) {
-        while (currentNode != root) {
-            currentNode.size--;
-            if (currentNode.size == 0) {
-                int ind = currentNode.sym;
-                currentNode = currentNode.parent;
-                currentNode.children[ind] = null;
-            } else {
-                currentNode = currentNode.parent;
-            }
-        }
-        currentNode.size--;
     }
 
     private Node goDown(Node currentNode, String str) {
@@ -118,14 +103,8 @@ public class StringSetImpl implements StringSet{
     }
 
     private static final class Node {
-        private int sym;
-        private Node parent;
         private int size;
         private boolean endOfString;
         private Node[] children = new Node[NUM_OF_SYM];
-        private Node() {}
-        private Node(int c) {
-            sym = c;
-        }
     }
 }
