82e9eaf03a4f94573b8846082e70c9ef8f4789d6
Charkin Kostya <93kostya@gmail.com>
2016-04-19 15:18:28 +0300

    test-02-impl

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,6 +1,10 @@
 package ru.spbau.mit;
 
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 
 public final class Injector {
@@ -12,6 +16,46 @@ public final class Injector {
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        Class aClass = classLoader.loadClass(rootClassName);
+        Constructor c = aClass.getConstructors()[0];
+        Class[] parameterTypes = c.getParameterTypes();
+
+        List<String> usingClass = new ArrayList<>();
+
+        List<Object> parameters = new ArrayList<>();
+
+        for (Class param : parameterTypes) {
+            boolean flag = false;
+            for(String className : implementationClassNames) {
+                Class tmp = Class.forName(className);
+                if (param.isAssignableFrom(tmp)) {
+                    if (flag) {
+                        throw new AmbiguousImplementationException();
+                    }
+                    usingClass.add(className);
+                    parameters.add(Injector.initialize(className, implementationClassNames));
+                    flag = true;
+                }
+            }
+        }
+
+        if (parameterTypes.length != parameters.size()) {
+            throw new ImplementationNotFoundException();
+        }
+
+        List<Object> parametersInRightOrder = new ArrayList<>();
+        int size = parameters.size();
+        for (int i = 0; i < size; i++) { //types
+            for (int j = 0; j < size; j++) { //objects
+                Class parameterType = parameterTypes[i];
+                if (parameterType.isAssignableFrom(parameters.get(j).getClass())) {
+                    parametersInRightOrder.add(parameters.get(j));
+                    break;
+                }
+            }
+        }
+
+        return c.newInstance(parametersInRightOrder.toArray());
     }
 }
