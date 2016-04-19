e9f0c95839a1a2329d1789bbd1bf09bc298a7807
Charkin Kostya <93kostya@gmail.com>
2016-04-19 15:42:16 +0300

    some fixes

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -2,38 +2,51 @@ package ru.spbau.mit;
 
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 
 public final class Injector {
-    private Injector() {
-    }
+    private static HashSet<String> markGlobal = new HashSet<>();
+    private static String rootClass = null;
 
     /**
      * Create and initialize object of `rootClassName` class using classes from
      * `implementationClassNames` for concrete dependencies.
      */
+
+    private Injector() {
+    }
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         Class aClass = classLoader.loadClass(rootClassName);
         Constructor c = aClass.getConstructors()[0];
         Class[] parameterTypes = c.getParameterTypes();
 
-        List<String> usingClass = new ArrayList<>();
+        if (rootClass == null) {
+            rootClass = rootClassName;
+        }
+
+        HashSet<String> markLocal = new HashSet<>();
+
+        markGlobal.add(aClass.getCanonicalName());
+
 
         List<Object> parameters = new ArrayList<>();
 
         for (Class param : parameterTypes) {
-            boolean flag = false;
             for (String className : implementationClassNames) {
                 Class tmp = Class.forName(className);
                 if (param.isAssignableFrom(tmp)) {
-                    if (flag) {
+                    if (markLocal.contains(className)) {
                         throw new AmbiguousImplementationException();
                     }
-                    usingClass.add(className);
+                    markLocal.add(className);
+
+                    if (markGlobal.contains(className)) {
+                        throw new InjectionCycleException();
+                    }
                     parameters.add(Injector.initialize(className, implementationClassNames));
-                    flag = true;
                 }
             }
         }
